6c415acb7422f37e85a5473c60340388b637d3bc
Swopnab <89373272+Swopnab@users.noreply.github.com>
2025-03-17 02:41:33 -0500

    initial commit

--- index.py
+++ index.py
@@ -31,6 +31,43 @@ def load_music():
     songlist.selection_set(0)
     current_song=song[songlist.curselection()[0]]
 
+def play_music():
+    global current_song, paused
+
+    if not paused:
+        pygame.mixer.music.load(os.path.join(root.directory, current_song))
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.unpause()
+        paused=False
+
+def pause_music():
+    global pause
+    pygame.mixer.music()
+    paused=True
+
+def next_music():
+    global current_song, paused
+
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song)+1)
+        current_song=songs[songlist.curselection()[0]]
+        play_music()
+    except:
+        pass
+
+def prev_music():
+    global current_song, paused
+
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song)-1)
+        current_song=songs[songlist.curselection()[0]]
+        play_music()
+    except:
+        pass
+
 organise_menu=Menu(menubar, tearoff=False)
 organise_menu.add_command(label='Select Folder', command=load_music)
 menubar.add_cascade(label='Organize', menu=organise_menu)
@@ -46,10 +83,10 @@ prev_btn_image = PhotoImage(file='previous.png')
 control_frame = Frame(root)
 control_frame.pack()
 
-play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
-pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
-next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
-prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0)
+play_btn = Button(control_frame, image=play_btn_image, borderwidth=0, command=play_music)
+pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0, command=pause_music)
+next_btn = Button(control_frame, image=next_btn_image, borderwidth=0, command=next_music)
+prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0, command=prev_music)
 
 play_btn.grid(row=0, column=1, pady=10, padx=7)
 pause_btn.grid(row=0, column=2, pady=10, padx=7)

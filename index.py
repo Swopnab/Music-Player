922bd8b6e0c5996262dd2acf578e7028ca17ab61
Swopnab <89373272+Swopnab@users.noreply.github.com>
2025-03-17 01:09:26 -0500

    initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import pygame
+import os
+
+root = Tk()
+root.title('Music Player')
+root.geometry("500x300")
+
+pygame.mixer.init()
+
+songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
+songlist.pack()
+
+root.mainloop()
